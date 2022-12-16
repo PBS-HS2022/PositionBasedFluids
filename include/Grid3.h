3e483609c6a63e1d68c55deb86ad37d3124e114a
Yuto Takano <moa17stock@gmail.com>
2022-12-16 04:08:27 +0100

    Remove unused getColors params and function body

--- include/Grid3.h
+++ include/Grid3.h
@@ -55,7 +55,7 @@ public:
 
 		Eigen::Vector3d Vmax = V.colwise().maxCoeff();
 		Eigen::Vector3d Vmin = V.colwise().minCoeff();
-		const Eigen::RowVector3i res = (Vmax - Vmin).cast<int>();
+		const Eigen::RowVector3i res = Eigen::RowVector3i(m_res_x, m_res_y, m_res_z);
 
 		Eigen::MatrixXd GV(res(0) * res(1) * res(2), 3);
 		Eigen::VectorXd points(res(0) * res(1) * res(2));
@@ -116,113 +116,16 @@ public:
 	}
 
 	void set_m_x(int x, int y, int z) {
-		// std::cout << m_res_x << " " << m_res_y << std::endl;
 		m_x(x, y, z) += 0.5;
 	}
 
-	void getColors(Eigen::MatrixXd& C, bool normalize=false, bool faceColor=true) const { 
+	void getColors(Eigen::MatrixXd& C) const {
+		// Unlike 2D where the mesh could contain parts that were blue and black,
+		// 3D meshes only represent the fluid part. So we can return a single
+		// value for the whole thing.
 		C.resize(1, 3);
 		C.row(0) = Eigen::RowVector3d(0, 0, 255);
 		return;
-		if (faceColor) {
-			if (C.rows() == 0) {
-				int num_faces = m_res_x * m_res_y * m_res_z * 2;
-				C = Eigen::MatrixXd(num_faces, 3);
-			}
-			int i = 0;
-			double cmin = m_x(0, 0, 0);
-			double cmax = cmin;
-			for (int z = 0; z < m_res_z; ++z) {
-				for (int y = 0; y < m_res_y; ++y) {
-					for (int x = 0; x < m_res_x; ++x) {
-						double c = m_x(x, y, z);
-						if (normalize) {
-							if (c > cmax) cmax = c;
-							if (c < cmin) cmin = c;
-						}
-						else {
-							for (int f = 0; f < 2; f++) {
-								// setting each of the 12 faces
-								if (c == 0.5) {
-									C.row(i++) = Eigen::RowVector3d(0, 0, 255);
-								}
-								else {
-									C.row(i++).setConstant(c);
-								}
-							}
-						}
-					}
-				}
-			}
-
-			if (!normalize) return;
-			else if (cmin == cmax) {
-				C.setZero();
-				return;
-			}
-
-			// std::cout << "cmin:" << cmin << " cmax:" << cmax << std::endl;
-			for (int z = 0; z < m_res_z; ++z) {
-				for (int y = 0; y < m_res_y; ++y) {
-					for (int x = 0; x < m_res_x; ++x) {
-						double c = m_x(x, y, 0);
-
-						c = (c - cmin) / (cmax - cmin); // [0,1]
-						double r, g, b;
-						igl::colormap(igl::COLOR_MAP_TYPE_VIRIDIS, c, r, g, b);
-						for (int f = 0; f < 2; f++) {
-							C.row(i++) = Eigen::RowVector3d(r, g, b);
-						}
-					}
-				}
-			}
-		}
-		else {
-			// vertex color
-			if (C.rows() == 0) {
-				int num_vertices = (m_res_x + 1) * (m_res_y + 1) * (m_res_z + 1);
-				C = Eigen::MatrixXd(num_vertices, 3);
-			}
-			int i = 0;
-			double cmin = m_x(0, 0, 0);
-			double cmax = cmin;
-			for (int y = 0; y <= m_res_y; ++y) {
-				for (int x = 0; x <= m_res_x; ++x) {
-					int x0 = std::max(x - 1, 0);
-					int x1 = std::min(x, m_res_x - 1);
-					int y0 = std::max(y - 1, 0);
-					int y1 = std::min(y, m_res_y - 1);
-
-					double c00 = m_x(x0, y0, 0);
-					double c01 = m_x(x0, y1, 0);
-					double c10 = m_x(x1, y0, 0);
-					double c11 = m_x(x1, y1, 0);
-					double c = (c00 + c01 + c10 + c11) / 4;
-					if (normalize) {
-						if (c > cmax) cmax = c;
-						if (c < cmin) cmin = c;
-					}
-					C.row(i++).setConstant(c);
-				}
-			}
-
-			if (!normalize) return;
-			else if (cmin == cmax) {
-				C.setZero();
-				return;
-			}
-
-			i = 0;
-			// std::cout << "cmin:" << cmin << " cmax:" << cmax << std::endl;
-			for (int y = 0; y <= m_res_y; ++y) {
-				for (int x = 0; x <= m_res_x; ++x) {
-					double c = (C(i, 0) - cmin) / (cmax - cmin); // [0,1]
-					double r, g, b;
-					igl::colormap(igl::COLOR_MAP_TYPE_VIRIDIS, c, r, g, b);
-					C.row(i++) = Eigen::RowVector3d(r, g, b);
-				}
-			}
-		}
 	}
 
 	inline int getResolutionX() const {
